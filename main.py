7012d36260167b2cbe13c025a9bcd7e2ad8a08db
Hamdi Taha <hamdi.taha@esprit.tn>
2024-07-17 21:12:19 +0200

    Adding fast api referencing

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import search_wiki
+from mylib.logic import wiki as wikilogic
+from mylib.logic import phrase as wikiphrases
 
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = FastAPI()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/")
+async def root():
+    return {"message": "Wikipedia API.  Call /search or /wiki"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Page to search in wikipedia"""
+
+    result = search_wiki(value)
+    return {"result": result}
+
+
+@app.get("/wiki/{name}")
+async def wiki(name: str):
+    """Retrieve wikipedia page"""
+
+    result = wikilogic(name)
+    return {"result": result}
+
+
+@app.get("/phrase/{name}")
+async def phrase(name: str):
+    """Retrieve wikipedia page and return phrases"""
+
+    result = wikiphrases(name)
+    return {"result": result}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
